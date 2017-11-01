d5224d6c0da3f53c0d2baee26c7e2a77a5707fe6
bwitoradyo <bambang.witoradyo@gmail.com>
2017-10-31 21:02:34 -0400

    JSX Expression

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,56 @@ console.log("busy running");
 
 //JSX - Javascript XML
 var template = React.createElement(
-  "h1",
+  "div",
   null,
-  "Decision App"
+  React.createElement(
+    "h1",
+    null,
+    "Sumologic"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is some info"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    )
+  )
 );
+
+//Create a templateTwo var JSX expression
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Sumologic"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: 26"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: Ridgefield Park"
+  )
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 console.log("busy running");
 
 //JSX - Javascript XML
-var template = <h1>Decision App</h1>;
+var template =(
+<div>
+  <h1>Sumologic</h1>
+  <p>This is some info</p>
+  <ol>
+  	<li>Item one</li>
+  	<li>Item two</li>
+  </ol>
+</div>
+);
+
+//Create a templateTwo var JSX expression
+
+const templateTwo = (
+  <div>
+  	<h1>Sumologic</h1>
+  	<p>Age: 26</p>
+  	<p>Location: Ridgefield Park</p>
+  </div>		
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
 
